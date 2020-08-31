6e658da5df20f38ac254d16f11ee52ba2ef141a4
Gabe Rosen <gmrosen7@gmail.com>
2020-08-31 12:34:00 -0400

    Number of teams no longer restricted to 4. Number of teams must be a mutiple of 48.

--- MarioExtravaganza.py
+++ MarioExtravaganza.py
@@ -1,4 +1,4 @@
-import random
+import random, math
 from typing import List, Dict
 from datetime import datetime
 
@@ -9,8 +9,11 @@ class MarioExtravaganza:
 	# and the value is the player name. Appends dictionary to list
 	def listBuild(self, names: str) -> List[dict]:
 		nameLst = names.replace(" ","").split(",")
+		teamCt = len(nameLst) / 2
 
-		if len(nameLst) == 8:
+		# validate that the team count is a multiple of 48
+		# throw exception if it isn't
+		if teamCt > 0 and 48 % teamCt == 0:
 			dictLst = list()
 
 			for i in range(0, len(nameLst), 2):
@@ -18,30 +21,36 @@ class MarioExtravaganza:
 				teamDict = {'0': team[0], '1': team[1]} 
 				dictLst.append(teamDict)
 		else:
-			raise Exception("There must be eight players!!")
+			raise Exception("The number of teams must be a multiple of 48!!")
 		
 		return dictLst
 
-	#uses binary values to assign players to a race
-	# returns 2d list of strings with player lineup
-	# for each race 
+	# generates every combination of races lineups
+	# these lineups are then mutliplied by a certain factor
+	# so that there are 48 total races, then races are shuffled
 	def lineupBuild(self, teams: List[dict]) -> List[List[str]]:
 		lineup = list()
-		
-		# loop through 16 binary variations
-		for i in range(0,16):
+		teamLen = len(teams)
+		combos = int(math.pow(2, teamLen))
+
+		# loop through all different number of comboes
+		for i in range(0, combos):
 			tmpLst = list()
-			binList = list('{0:04b}'.format(i))
+			# create list of binary values to represent 
+			# current combinary
+			binList = list('{:0{}b}'.format(i, teamLen))
 
 			# select player from each team based on binary value
-			for j in range(0,4):
+			for j in range(0, teamLen):
 				player = teams[j][binList[j]]
 				tmpLst.append(player)
 			
 			lineup.append(tmpLst)
 		
-		# multiply list by 3 and then shuffle
-		lineup *= 3
+		# multiply list by factor needed to make 48 races
+		# then shuffle races
+		factor = 48 // len(lineup)
+		lineup *= factor
 		random.shuffle(lineup)
 		return lineup
 
